39147b6e44163b5dc037240904ced7164dad47c6
gaurav <gagu12ec@gmail.com>
2016-10-16 22:12:29 +0530

    Prime number using Sieve of Eratosthenes method

--- ProjectSort/Sorting/src/com/gaurav/sortingAlgorithm/Sort.java
+++ ProjectSort/Sorting/src/com/gaurav/sortingAlgorithm/Sort.java
@@ -43,8 +43,54 @@ public class Sort {
     public static int[] quick(int[] array){
         return array;
     }
+    
     public static int[] merge(int[] array){
-        return array;
+        public void sort(int inputArr[]) {
+        this.array = inputArr;
+        this.length = inputArr.length;
+        this.tempMergArr = new int[length];
+        doMergeSort(0, length - 1);
+    }
+ 
+    private void doMergeSort(int lowerIndex, int higherIndex) {
+         
+        if (lowerIndex < higherIndex) {
+            int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
+            // Below step sorts the left side of the array
+            doMergeSort(lowerIndex, middle);
+            // Below step sorts the right side of the array
+            doMergeSort(middle + 1, higherIndex);
+            // Now merge both sides
+            mergeParts(lowerIndex, middle, higherIndex);
+        }
+    }
+ 
+    private void mergeParts(int lowerIndex, int middle, int higherIndex) {
+ 
+        for (int i = lowerIndex; i <= higherIndex; i++) {
+            tempMergArr[i] = array[i];
+        }
+        int i = lowerIndex;
+        int j = middle + 1;
+        int k = lowerIndex;
+        while (i <= middle && j <= higherIndex) {
+            if (tempMergArr[i] <= tempMergArr[j]) {
+                array[k] = tempMergArr[i];
+                i++;
+            } else {
+                array[k] = tempMergArr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i <= middle) {
+            array[k] = tempMergArr[i];
+            k++;
+            i++;
+        }
+ 
+    }
+    return array;
     }
     public static int[] heap(int[] array){
         return array;
